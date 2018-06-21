38f2203c4f4b8bfb89c8d5c7fb1302a3ec6835bf
Tom Huang <tom6311tom6311@gmail.com>
2018-06-21 11:04:37 +0800

    fix: txt-to-speech authentication
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- txt2speech.py
+++ txt2speech.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*
 
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Desktop/txt2speech.json"
+
 def synthesize_text(text):
     """Synthesizes speech from the input string of text."""
-    from google.cloud import texttospeech
-    client = texttospeech.TextToSpeechClient()
+    from google.cloud import texttospeech_v1beta1
+    client = texttospeech_v1beta1.TextToSpeechClient()
 
-    input_text = texttospeech.types.SynthesisInput(text=text)
+    input_text = texttospeech_v1beta1.types.SynthesisInput(text=text)
 
     # Note: the voice can also be specified by name.
     # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='zh-Hant',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+    voice = texttospeech_v1beta1.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech_v1beta1.enums.SsmlVoiceGender.FEMALE)
 
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    audio_config = texttospeech_v1beta1.types.AudioConfig(
+        audio_encoding=texttospeech_v1beta1.enums.AudioEncoding.MP3)
 
     response = client.synthesize_speech(input_text, voice, audio_config)
 
@@ -24,4 +28,4 @@ def synthesize_text(text):
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
 
-synthesize_text('妳好')
+synthesize_text('hello')
